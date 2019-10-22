11e81e43d0988f3f3925d70675f3ab05fcc63c65
Masaya Kataoka <ms.kataoka@gmail.com>
2019-10-22 13:49:50 +0900

    add pcd_writer component

---
+++ src/io/pcd_writer/pcd_writer_component.cpp
@@ -0,0 +1,10 @@
+#include <pcl_apps/io/pcd_writer/pcd_writer_component.h>
+
+namespace pcl_apps
+{
+    PcdWriterComponent::PcdWriterComponent(const rclcpp::NodeOptions & options)
+    : Node("points_transform", options)
+    {
+
+    }
+}
---
+++ src/io/pcd_writer/pcd_writer_node.cpp
@@ -0,0 +1,14 @@
+// Headers in this package
+#include <pcl_apps/io/pcd_writer/pcd_writer_component.h>
+// Headers in RCLCPP
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions options;
+    auto component = std::make_shared<pcl_apps::PcdWriterComponent>(options);
+    rclcpp::spin(component);
+    rclcpp::shutdown();
+    return 0;
+}
