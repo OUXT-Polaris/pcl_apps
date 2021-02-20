baec4b7733303349d5ec4042c3ec45b1f81c6535
Masaya Kataoka <ms.kataoka@gmail.com>
2021-02-20 23:58:46 +0900

    add compile definition
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- pcl_apps/include/pcl_apps/projection/pointcloud_projection/pointcloud_projection_component.hpp
+++ pcl_apps/include/pcl_apps/projection/pointcloud_projection/pointcloud_projection_component.hpp
@@ -15,4 +15,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if __cplusplus
+extern "C" {
+#endif
+
+// The below macros are taken from https://gcc.gnu.org/wiki/Visibility and from
+// demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef __GNUC__
+#define PCL_APPS_POINTCLOUD_PROJECTION_EXPORT __attribute__((dllexport))
+#define PCL_APPS_POINTCLOUD_PROJECTION_IMPORT __attribute__((dllimport))
+#else
+#define PCL_APPS_POINTCLOUD_PROJECTION_EXPORT __declspec(dllexport)
+#define PCL_APPS_POINTCLOUD_PROJECTION_IMPORT __declspec(dllimport)
+#endif
+#ifdef PCL_APPS_POINTCLOUD_PROJECTION_BUILDING_DLL
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC PCL_APPS_POINTCLOUD_PROJECTION_EXPORT
+#else
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC PCL_APPS_POINTCLOUD_PROJECTION_IMPORT
+#endif
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC_TYPE PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC
+#define PCL_APPS_POINTCLOUD_PROJECTION_LOCAL
+#else
+#define PCL_APPS_POINTCLOUD_PROJECTION_EXPORT __attribute__((visibility("default")))
+#define PCL_APPS_POINTCLOUD_PROJECTION_IMPORT
+#if __GNUC__ >= 4
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC __attribute__((visibility("default")))
+#define PCL_APPS_POINTCLOUD_PROJECTION_LOCAL __attribute__((visibility("hidden")))
+#else
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC
+#define PCL_APPS_POINTCLOUD_PROJECTION_LOCAL
+#endif
+#define PCL_APPS_POINTCLOUD_PROJECTION_PUBLIC_TYPE
+#endif
+
+#if __cplusplus
+}  // extern "C"
+#endif
+
 #endif  // PCL_APPS__PROJECTION__POINTCLOUD_PROJECTION__POINTCLOUD_PROJECTION_COMPONENT_HPP_
