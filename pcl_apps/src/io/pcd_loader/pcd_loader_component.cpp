6067484bf93e133ccc950f84920f2990b6e9c23e
asa-naki <aki.grade2580@outlook.jp>
2022-03-10 14:04:59 +0900

    fix pcd_loader for rviz visualization

--- pcl_apps/src/io/pcd_loader/pcd_loader_component.cpp
+++ pcl_apps/src/io/pcd_loader/pcd_loader_component.cpp
@@ -26,15 +26,20 @@ PcdLoaderComponent::PcdLoaderComponent(const rclcpp::NodeOptions & options)
 : Node("pcd_loader", options)
 {
   std::string file_path;
-  declare_parameter("file_path", "");
+  declare_parameter("file_path", std::string(""));
   get_parameter("file_path", file_path);
   std::string output_topic;
   declare_parameter("output_topic", get_name() + std::string("/output"));
+  get_parameter("output_topic", output_topic);
+  std::string inference_id;
+  declare_parameter("inference_id", std::string("map"));
+  get_parameter("inference_id", inference_id);
   pcl::PCLPointCloud2 cloud;
   int result = pcl::io::loadPCDFile(file_path, cloud);
   if (result == 0) {
     sensor_msgs::msg::PointCloud2 msg;
     pcl_conversions::fromPCL(cloud, msg);
+    msg.header.frame_id = inference_id;
     pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(
       output_topic, rclcpp::QoS(10).transient_local());
     pub_->publish(msg);
